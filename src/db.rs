bdcc6b7b8008fb8489ccb3e9ec669e145b2b74d4
JohnBakhmat <johnbakhmat@gmail.com>
2023-08-23 13:41:27 +0300

    Added subs

--- src/db.rs
+++ src/db.rs
@@ -1,5 +1,5 @@
 use crate::feed_parser::{self, Post};
-use crate::prisma::{feed, post, PrismaClient};
+use crate::prisma::{feed, post, subscription, user, PrismaClient};
 use std::vec;
 
 pub async fn add_post(feed_id: String, post: &Post) -> anyhow::Result<()> {
@@ -27,7 +27,7 @@ pub async fn add_post(feed_id: String, post: &Post) -> anyhow::Result<()> {
     Ok(())
 }
 
-pub async fn save_feed_to_db(feed: &feed_parser::Feed) -> anyhow::Result<()> {
+pub async fn find_or_upsert_feed(feed: &feed_parser::Feed) -> anyhow::Result<feed::Data> {
     let client = PrismaClient::_builder().build().await?;
 
     let feed_exists = client
@@ -37,7 +37,7 @@ pub async fn save_feed_to_db(feed: &feed_parser::Feed) -> anyhow::Result<()> {
         .await?;
 
     if feed_exists.is_some() {
-        let feed_id = feed_exists.unwrap().id;
+        let feed_id = &feed_exists.as_ref().unwrap().id;
         let posts = client
             .post()
             .find_many(vec![post::feed_id::equals(Some(feed_id.to_string()))])
@@ -60,46 +60,100 @@ pub async fn save_feed_to_db(feed: &feed_parser::Feed) -> anyhow::Result<()> {
         }
 
         if new_posts.len() == 0 {
-            return Ok(());
+            return Ok(feed_exists.unwrap());
         }
 
         for post in new_posts {
             add_post(feed_id.to_string(), post).await?;
         }
-        return Ok(());
+    } else {
+        let new_feed = client
+            .feed()
+            .create(feed.title.to_string(), feed.link.to_string(), vec![])
+            .exec()
+            .await?;
+
+        client
+            .post()
+            .create_many(
+                feed.items
+                    .iter()
+                    .map(|x| {
+                        post::create_unchecked(
+                            x.title.to_string(),
+                            x.link.to_string(),
+                            x.pub_date.to_string(),
+                            vec![post::feed_id::set(Some(new_feed.id.to_string()))],
+                        )
+                    })
+                    .collect(),
+            )
+            .exec()
+            .await?;
     }
 
-    client
+    let updated_feed = client
         .feed()
-        .create(feed.title.to_string(), feed.link.to_string(), vec![])
+        .find_first(vec![feed::id::equals(feed_exists.unwrap().id.to_string())])
         .exec()
         .await?;
 
-    let feed_id = client
-        .feed()
-        .find_first(vec![feed::link::equals(feed.link.to_string())])
-        .exec()
-        .await?
-        .unwrap()
-        .id;
+    return Ok(updated_feed.unwrap());
+}
 
-    client
-        .post()
-        .create_many(
-            feed.items
-                .iter()
-                .map(|x| {
-                    post::create_unchecked(
-                        x.title.to_string(),
-                        x.link.to_string(),
-                        x.pub_date.to_string(),
-                        vec![post::feed_id::set(Some(feed_id.to_string()))],
-                    )
-                })
-                .collect(),
+pub async fn subscribe_user_to_feed(
+    username: &str,
+    feed: &feed_parser::Feed,
+) -> anyhow::Result<subscription::Data> {
+    let user = find_or_create_user(username).await?;
+    let feed = find_or_upsert_feed(feed).await?;
+
+    let client = PrismaClient::_builder().build().await?;
+
+    let user_feed = client
+        .subscription()
+        .find_first(vec![
+            subscription::user_id::equals(user.id.to_string()),
+            subscription::feed_id::equals(feed.id.to_string()),
+        ])
+        .exec()
+        .await?;
+
+    if user_feed.is_some() {
+        return Ok(user_feed.unwrap());
+    }
+
+    let new_sub = client
+        .subscription()
+        .create(
+            user::id::equals(user.id.to_string()),
+            feed::id::equals(feed.id.to_string()),
+            vec![],
         )
         .exec()
         .await?;
 
-    Ok(())
+    Ok(new_sub)
+}
+
+pub async fn find_or_create_user(username: &str) -> anyhow::Result<user::Data> {
+    let client = PrismaClient::_builder().build().await?;
+
+    let user_exists = client
+        .user()
+        .find_first(vec![user::username::equals(username.to_string())])
+        .exec()
+        .await?;
+
+    if user_exists.is_some() {
+        return Ok(user_exists.unwrap());
+    }
+
+    let user = client
+        .user()
+        .create(username.to_string(), vec![])
+        .exec()
+        .await?;
+
+    Ok(user)
 }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,12 @@ mod prisma;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let nexxel = feed_parser::get_feed_by_url("https://nexxel.dev/rss.xml").await?;
-    db::save_feed_to_db(&nexxel).await?;
+    handle_sub("test", "https://nexxel.dev/rss.xml").await?;
+    Ok(())
+}
+
+async fn handle_sub(username: &str, url: &str) -> anyhow::Result<()> {
+    let feed = feed_parser::get_feed_by_url(url).await?;
+    db::subscribe_user_to_feed(username, &feed).await?;
     Ok(())
 }
